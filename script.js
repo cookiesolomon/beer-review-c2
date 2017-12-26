bccc0e7f17fd5b948fc691817ac4ecaabd47b3d2
Cookie Solomon <shoshsolomon0@gmail.com>
2017-12-26 12:03:36 +0200

    updates 26.12.17

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-//PROJECT EXERCISE 1
-// Inside script.js, declare an addBeer function that has two parameters, "name" and "category". 
-//Stop there with an empty function as we'll continue writing it in the next exercise.
-// Now, separately, create a function to handle the '.post-beer' button getting clicked.
-// Inside, the handler function you'll want to invoke addBeer. 
-//As it's arguments you'll need to give it the beer's "name" and "category" from the inputs.
-
-
-// PROJECT EXERCISE 2
-// In your JavaScript, create a global array called beers.
-// Inside the addBeer function, create a new beer object 
-// using the data that is passed to the function (ie via the parameters) and push it to a beers array.
-// PROJECT EXERCISE 3
-// Declare another function called renderBeers that takes no arguments.
-// Inside this function, firstly empty the beers <ul> so that it no longer has any <li> elements .
-//  Then loop through the beers array, inside the loop append the beers to the beers <ul>
-// Invoke renderBeers inside your click handler just below where you're invoking the addBeer function.
-// Why are we updating the beers after adding the new beer?
-
 var beers = [];
-
+var descending = true;
 
 function addBeer(name, category) {
     var beer = { 'name': name, 'category': category, };
@@ -26,45 +7,30 @@ function addBeer(name, category) {
     if ($('.rating').val()) {
         beer.rating = $('.rating').val();
     }
-
 }
 
-$('.post-beer').click(function () {
+function beerPost() {
     addBeer($('.beer-input').val(), $('.category-input').val());
     renderBeers();
     $('.beer-input').val("");
     $('.category-input').val("");
-});
 
-
-
+}
 function renderBeers() {
     $('.beers').find('li').remove();
     for (i = 0; i < beers.length; i++) {
-        $('.beers').append('<li>Beer Name: ' + beers[i].name + ' Beer Type: ' + beers[i].category + ' Rating: ' + beers[i].rating + '</li>');
+        $('.beers').append('<li><span class="bName">Beer Name: </span>' + beers[i].name + '<span class="cName">Beer Type:</span> ' + beers[i].category + ' <span class="rName">Rating:</span> ' + beers[i].rating + '</li>');
 
     }
-    beers.push()
-
 }
-
-// Add another button to your page that will sort the beers on the page by rating when clicked. 
-// If it gets clicked again, reverse the order (if they were sorted in ascending order, sort in descending and vice-versa).
 
 function sortByRating() {
-
     var newList = beers.sort(function (a, b) {
         return a.rating - b.rating;
-
     });
-
 }
 
-
-var descending = true;
-
-$('.sort-beer').click(function () {
-
+function sortButton() {
     sortByRating();
     if (descending) {
         reverseOrder();
@@ -75,13 +41,32 @@ $('.sort-beer').click(function () {
         descending = true;
     }
 
-
-
-});
-
+}
 function reverseOrder() {
     $('.beers').find('li').remove();
     for (i = beers.length - 1; i >= 0; i--) {
-        $('.beers').append('<li>Beer Name: ' + beers[i].name + ' Beer Type: ' + beers[i].category + ' Rating: ' + beers[i].rating + '</li>');
+        $('.beers').append('<li><span class="bName">Beer Name:</span>' + beers[i].name + ' <span class="cName">Beer Type:</span> ' + beers[i].category + ' <span class="rName">Rating:</span> ' + beers[i].rating + '</li>');
     }
 }
+
+// $('.post-beer').click(function () {
+//     addBeer($('.beer-input').val(), $('.category-input').val());
+
+//     renderBeers();
+
+//     $('.beer-input').val("");
+//     $('.category-input').val("");
+// });
+
+// $('.sort-beer').click(function () {
+
+//     sortByRating();
+//     if (descending) {
+//         reverseOrder();
+//         descending = false;
+
+//     } else {
+//         renderBeers();
+//         descending = true;
+//     }
+// });
